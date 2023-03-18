03c3575ab71f65099a8d24fe357c8f9a3cc097f8
Mitul Patel <md.ghoda@gmail.com>
2023-03-18 22:37:07 +0530

    Plan changes

--- app/src/main/java/com/app/cityshow/ui/adapter/PlanListAdapter.kt
+++ app/src/main/java/com/app/cityshow/ui/adapter/PlanListAdapter.kt
@@ -5,6 +5,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.app.cityshow.databinding.RowSubscriptionPlanBinding
 import com.app.cityshow.model.subscription.Plan
+import com.app.cityshow.utility.LocalDataHelper
+import com.app.cityshow.utility.hide
+import com.app.cityshow.utility.show
 
 class PlanListAdapter(
     var mArrayList: ArrayList<Plan>,
@@ -20,7 +23,18 @@ class PlanListAdapter(
     override fun onBindViewHolder(holder: CategoryHolder, position: Int) {
         val data = mArrayList[position]
         holder.bind(data)
-        holder.itemView.setOnClickListener {
+        if (data.id.equals(LocalDataHelper.user?.subscription?.plan_stripe_id, true)) {
+            holder.binding.layMain.isSelected = true
+            holder.binding.tvActivePlan.show()
+            holder.binding.layMain.isClickable = false
+            holder.binding.layMain.isEnabled = false
+        } else {
+            holder.binding.tvActivePlan.hide()
+            holder.binding.layMain.isClickable = true
+            holder.binding.layMain.isSelected = false
+            holder.binding.layMain.isEnabled = true
+        }
+        holder.binding.layMain.setOnClickListener {
             onClickItem.invoke(data, 1)
         }
 
